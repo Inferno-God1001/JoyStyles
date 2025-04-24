b490aed6c3f82072cba72d991a837201b3b819b9
DimenCoder <renangameslinha8@gmail.com>
2025-04-23 22:38:06 -0300

    Atualizar o prefix.js

--- Bots/BoaHancock/prefix.js
+++ Bots/BoaHancock/prefix.js
@@ -1,14 +1,14 @@
 const palavrasParaDestacar = ['**Boa-Hancock:**', '**boa-hancock:**', '**boahancock:**', '**BoaHancock:**']; // palavras que vão ficar braba
     const classeDaDiv = 'minha-div'; // a classe da div onde vai rolar a mágica
 
-    const divs = document.querySelectorAll(`.`);
+    const divs = document.querySelectorAll(`.body-text.robot-text.last-text`);
 
     divs.forEach(div => {
       let textoOriginal = div.innerHTML;
 
       palavrasParaDestacar.forEach(palavra => {
         const regex = new RegExp(`\\b(${palavra})\\b`, 'gi');
-        textoOriginal = textoOriginal.replace(regex, `<span class="destaque">$1</span>`);
+        textoOriginal = textoOriginal.replace(regex, `<span class="prefixx">$1</span>`);
       });
 
       div.innerHTML = textoOriginal;
