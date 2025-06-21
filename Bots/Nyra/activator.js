f51b7f0cecf8a791a1288c8c848928234dff46d3
DimenCoder <renangameslinha8@gmail.com>
2025-06-21 01:58:44 -0300

    Atualizar o activator.js

--- Bots/Nyra/activator.js
+++ Bots/Nyra/activator.js
@@ -1,12 +1,11 @@
 function themeButton() {
-      // Verifica se o CSS já foi carregado para evitar duplicação
-      if (!document.getElementById('tema-css')) {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = 'https://inferno-god1001.github.io/JoyStyles/Bots/BoaHancock/indexJL.css'; // Substitua com o caminho do seu CSS
-        link.id = 'tema-css';
-        document.head.appendChild(link);
-      } else {
-        alert("O tema já foi carregado.");
-      }
-    }
+  if (!document.getElementById('tema-css')) {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = 'https://inferno-god1001.github.io/JoyStyles/Bots/BoaHancock/indexJL.css'; // coloque o link correto do seu tema.css
+    link.id = 'tema-css';
+    document.head.appendChild(link);
+  } else {
+    console.log("Tema já carregado.");
+  }
+}
