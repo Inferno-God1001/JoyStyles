958a0f6f13694b420cb01c2fedc9c10d7d5fa52b
DimenCoder <renangameslinha8@gmail.com>
2025-06-23 03:00:37 -0300

    Criar antiDelay.js

---
+++ Bots/Nyra/antiDelay.js
@@ -0,0 +1,4 @@
+
+  const css = document.getElementById("dynamic-css");
+  const timestamp = new Date().getTime(); // Gera um número único
+  css.href = "https://inferno-god1001.github.io/JoyStyles/Bots/Nyra/basis.css?v=" + timestamp;
