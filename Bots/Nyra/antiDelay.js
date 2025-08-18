5e53fd9f97e498012c191803751c72202d29ebab
DimenCoder <renangameslinha8@gmail.com>
2025-08-18 12:45:16 -0300

    Delete Bots/Nyra/antiDelay.js

--- Bots/Nyra/antiDelay.js
+++
@@ -1,4 +0,0 @@
-
-  const css = document.getElementById("dynamic-css");
-  const timestamp = new Date().getTime(); // Gera um número único
-  css.href = "https://inferno-god1001.github.io/JoyStyles/Bots/Nyra/basis.css?v=" + timestamp;
