77952bd5582b9827d8fecd0918bebe5ead053833
DimenCoder <renangameslinha8@gmail.com>
2025-08-18 12:45:57 -0300

    Delete Bots/BoaHancock/noBug.js

--- Bots/BoaHancock/noBug.js
+++
@@ -1,9 +0,0 @@
-let link1 = document.createElement('link');
-link1.rel = 'stylesheet';
-link1.href = 'https://inferno-god1001.github.io/JoyStyles/Bots/BoaHancock/indexJL.css';
-document.head.appendChild(link1);
-
-let link2 = document.createElement('link');
-link2.rel = 'stylesheet';
-link2.href = 'https://inferno-god1001.github.io/JoyStyles/BoaHancock/message.css';
-document.head.appendChild(link2);
